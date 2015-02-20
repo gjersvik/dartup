e673d392ee8f59f2ac06070cf09704e0d8fbacda
Ole Martin Gjersvik <olem@olem.org>
2015-02-20 16:20:06 +0100

    Have redirected to github and back.

--- proto/github_signin.dart
+++ proto/github_signin.dart
@@ -1,5 +1,22 @@
+library dartup_prototype_github_signin;
+
+import "dart:html";
+
 import "package:dartup/secret.dart" as secret;
 
 main(){
-  print(secret.githubClinetId);
+  print(window.location.search);
+  
+  querySelector("#signin").onClick.listen((_){
+    
+    var req = {
+               "client_id": secret.githubClinetId,
+               "redirect_uri": "http://localhost:8080/github_signin.html",
+               "scope": "user:email"
+               
+    };
+    var uri = new Uri.https("github.com", "/login/oauth/authorize",req);
+    
+    window.location.assign(uri.toString());
+  });
 }
