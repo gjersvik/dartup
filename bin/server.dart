d0f8cdece1bc1216f83b89c98fb8f8a39b543760
Ole Martin Gjersvik <olem@olem.org>
2015-03-01 19:50:08 +0100

    Started on auth on server.

--- bin/server.dart
+++ bin/server.dart
@@ -1,13 +1,26 @@
 library dartup_controll;
 
-import "package:redstone/server.dart" as app;
+import "dart:async";
 
-part "src/ping.dart";
+import "package:redstone/server.dart" as app;
+import "package:di/di.dart";
+
+part "src/interceptor/auth.dart";
+part "src/routers/ping.dart";
 
 @app.Route("/")
 helloWorld() => "Hello, World!";
 
+typedef Future<bool> AuthFunction(String token);
+
+Future<bool> auth(String token){
+  return new Future.value(false);
+}
+
 main(List<String> args){
+  app.addModule(new Module()
+    ..bind(AuthFunction, toInstanceOf: auth));
+  
   app.setupConsoleLog();
   app.start();
 }
---
+++ bin/src/interceptor/auth.dart
@@ -0,0 +1,1 @@
+part of dartup_controll;
