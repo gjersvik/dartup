39f3bbefc729c3dc48ea933e22fca014ae656fd6
Ole Martin Gjersvik <olem@olem.org>
2015-03-08 20:52:44 +0100

    Get some debug infro from dart_coveralls.

--- tool/travis.sh
+++ tool/travis.sh
@@ -10,6 +10,7 @@ dart --checked test/test_all.dart
 if [ "$COVERALLS_TOKEN" ]; then
   pub global activate dart_coveralls
   pub global run dart_coveralls report \
+    --debug \
     --token $COVERALLS_TOKEN \
     --retry 2 \
     --exclude-test-files \
