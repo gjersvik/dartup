57885daea992d5b403ab915fe78a9ee44bbc67cc
Ole Martin Gjersvik <olem@olem.org>
2015-03-19 10:25:18 +0100

    Implemented  the user class.

--- lib/src/user.dart
+++ lib/src/user.dart
@@ -1,5 +1,23 @@
 part of dartup_common;
 
 class User{
+  int id;
+  String email;
+  bool active;
 
+  User.fromJson(Map json){
+    id = json["id"];
+    email = json["email"];
+    active = json["active"];
+  }
+
+  Map toJson(){
+    return {
+      "id": id,
+      "email": email,
+      "active": active
+    };
+  }
+
+  String toString() => "User(id:$githubId, email:$email, active:$active)";
 }
